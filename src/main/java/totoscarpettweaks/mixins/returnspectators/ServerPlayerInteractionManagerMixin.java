3ccb8d9665d854290b0e6a63b05bf94814b62499
Totorewa <totorewa@outlook.com>
2022-12-10 23:42:10 +1300

    Fix returnSpectators for 1.19.3
    The previous game mode doesn't seem to change in 1.19.3 as
    it did in the previous version, where it now sets the value
    of the previous game mode to itself for some reason.
    
    Injection point is moved to before the game mode field is
    updated and uses the field and method argument to instead
    determine what game mode is being changed from and to.

--- src/main/java/totoscarpettweaks/mixins/returnspectators/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/totoscarpettweaks/mixins/returnspectators/ServerPlayerInteractionManagerMixin.java
@@ -1,8 +1,6 @@
 package totoscarpettweaks.mixins.returnspectators;
 
 import carpet.patches.EntityPlayerMPFake;
-import totoscarpettweaks.TotoCarpetSettings;
-import totoscarpettweaks.fakes.ServerPlayerEntityInterface;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.world.GameMode;
@@ -10,21 +8,31 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import totoscarpettweaks.TotoCarpetSettings;
+import totoscarpettweaks.fakes.ServerPlayerEntityInterface;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public abstract class ServerPlayerInteractionManagerMixin {
     @Shadow
     public ServerPlayerEntity player;
 
-    @Inject(method = "setGameMode(Lnet/minecraft/world/GameMode;Lnet/minecraft/world/GameMode;)V", at = @At("HEAD"))
-    private void onGameModeChange(GameMode gameMode, GameMode previousGameMode, CallbackInfo ci) {
-        if (TotoCarpetSettings.returnSpectators && gameMode != previousGameMode && !(player instanceof EntityPlayerMPFake)) {
+    @Shadow
+    private GameMode gameMode;
+
+    @Inject(
+            method = "changeGameMode(Lnet/minecraft/world/GameMode;)Z",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/network/ServerPlayerInteractionManager;setGameMode(Lnet/minecraft/world/GameMode;Lnet/minecraft/world/GameMode;)V",
+                    shift = At.Shift.BEFORE))
+    private void onGameModeChange(GameMode gameMode, CallbackInfoReturnable<Boolean> cir) {
+        if (TotoCarpetSettings.returnSpectators && !(player instanceof EntityPlayerMPFake)) {
             // If changing from survival mode, remember position
             if (gameMode == GameMode.SURVIVAL) {
                 ((ServerPlayerEntityInterface) player).tryReturnToSurvivalPosition();
-            // If changing to survival mode, teleport to previous survival position
-            } else if (previousGameMode == GameMode.SURVIVAL) {
+                // If changing to survival mode, teleport to previous survival position
+            } else if (this.gameMode == GameMode.SURVIVAL) {
                 ((ServerPlayerEntityInterface) player).rememberSurvivalPosition();
             }
         }
